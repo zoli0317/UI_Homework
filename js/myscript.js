8f4d19d20fe80805f37a05ad7da792482439547c
zoli0317 <zoli0317@freemail.hu>
2016-04-17 19:40:21 +0200

    Fixed arrow working
    Fixed arrow working

--- js/myscript.js
+++ js/myscript.js
@@ -1,19 +1,10 @@
 $(document).ready(function(){
-    $("#secondHotels").hide();
-    $("#thirdHotels").hide();
+    $('#secondHotel').hide();
+    $('#thirdHotel').hide();
 
-    $("#firstDestination").click(function(){
-        $("#firstHotels").slideToggle("slow");
-		$("#arrowOpen1").attr("src", "assets/arrow-3.png");
-    });
-	
-	$("#secondDestination").click(function(){
-        $("#secondHotels").slideToggle("slow");
-		$("#arrowOpen2").attr("src", "assets/arrow-2.png");
-    });
-	
-	$("#thirdDestination").click(function(){
-        $("#thirdHotels").slideToggle("slow");
-		$("#arrowOpen3").attr("src", "assets/arrow-2.png");
-    });	
+	$('.destination').on('click', function () {
+		$(this).children('.arrow').toggleClass('arrowClose');
+		$(this).children('.arrow').toggleClass('arrowOpen');
+		$(this).siblings('.hotel').slideToggle("slow");
+	});
 });
